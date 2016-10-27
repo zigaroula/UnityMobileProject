c2f4cddc401b33d2f0ae7133937abb0a8a86b0a3
Zigaroula <benjaminbontemps@gmail.com>
2016-10-27 17:58:55 +0200

    new bg, stars, ship moving properly

---
+++ Assets/Scripts/Decorum/StarsHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarsHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LateUpdate () {
+		float speed = GameManager.manager.GetGameSpeed ();
+		// Update particle velocities, assumes constant velocity
+		int numParticles = gameObject.GetComponent<ParticleSystem> ().particleCount;
+		ParticleSystem.Particle[] particles = new ParticleSystem.Particle[numParticles];
+		gameObject.GetComponent<ParticleSystem> ().GetParticles(particles);
+		//print (particles [0].velocity);
+		for (int i = 0; i < particles.Length; i++) {
+			particles[i].velocity = new Vector3(0.0f, 0.0f, 10.0f * speed);
+		}
+		gameObject.GetComponent<ParticleSystem> ().SetParticles(particles, numParticles);
+		ParticleSystem.EmissionModule em = gameObject.GetComponent<ParticleSystem> ().emission;
+		em.rate = 10.0f * speed;
+	}
+}
--- Assets/Scripts/Player/ShipMove.cs
+++ Assets/Scripts/Player/ShipMove.cs
@@ -5,18 +5,23 @@ public class ShipMove : MonoBehaviour {
 
 	public float MovementSpeed;
 	private Vector3 initialPosition;
+	private float xTar;
 
 	// Use this for initialization
 	void Start () {
 		initialPosition = transform.position;
+		xTar = 0.5f;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		//KeyboardDebug ();
-		//TouchControl();
-		AlternateKeyboardDebug();
-		//AlternateTouchControl();
+		float x;
+		x = AlternateKeyboardDebug ();
+		//x = AlternateTouchControl ();
+		if (x > -10) {
+			xTar = x;
+		}
+		ProceedMovement (xTar);
 		ClampPositionToScreen ();
 	}
 
@@ -32,19 +37,15 @@ public class ShipMove : MonoBehaviour {
 		}
 	}
 
-	private void AlternateKeyboardDebug() {
+	private float AlternateKeyboardDebug() {
 		if (GameManager.manager.GetCurrentGameState () == GameManager.GameState.Game) {
 			float x = Input.mousePosition.x / Screen.width;
 			float y = Input.mousePosition.y / Screen.height;
-			float speed = GameManager.manager.GetGameSpeed();
-			float xVelocity = 0.0f;
-			float currentX = transform.position.x;
-			float finalX = (x * 6.0f) - 3.0f;
-			float targetX = Mathf.SmoothDamp (currentX, finalX, ref xVelocity, 0.1f/speed);
-			transform.position = new Vector3 (targetX, transform.position.y, transform.position.z);
-			float rotationAngle = 90.0f * (currentX-finalX)/6.0f;
-			transform.eulerAngles = new Vector3 (transform.eulerAngles.x, rotationAngle, transform.eulerAngles.z);
+			if (y <= 0.9) {
+				return x;
+			}
 		}
+		return -10;
 	}
 
 	private void TouchControl() {
@@ -61,22 +62,29 @@ public class ShipMove : MonoBehaviour {
 		}
 	}
 
-	private void AlternateTouchControl() {
+	private float AlternateTouchControl() {
 		if (GameManager.manager.GetCurrentGameState () == GameManager.GameState.Game) {
 			if (Input.touchCount == 1) {
 				Touch touch = Input.GetTouch (0);
 				float x = touch.position.x / Screen.width;
 				float y = touch.position.y / Screen.height;
-				float speed = GameManager.manager.GetGameSpeed();
-				float xVelocity = 0.0f;
-				float currentX = transform.position.x;
-				float finalX = (x * 6.0f) - 3.0f;
-				float targetX = Mathf.SmoothDamp (currentX, finalX, ref xVelocity, 0.1f/speed);
-				transform.position = new Vector3 (targetX, transform.position.y, transform.position.z);
-				float rotationAngle = 90.0f * (currentX-finalX);
-				transform.eulerAngles = new Vector3 (transform.eulerAngles.x, rotationAngle, transform.eulerAngles.z);
+				if (y <= 0.9) {
+					return x;
+				}
 			}
 		}
+		return -10;
+	}
+
+	private void ProceedMovement(float x) {
+		float speed = GameManager.manager.GetGameSpeed();
+		float xVelocity = 0.0f;
+		float currentX = transform.position.x;
+		float finalX = (x * 6.0f) - 3.0f;
+		float targetX = Mathf.SmoothDamp (currentX, finalX, ref xVelocity, 0.1f/speed);
+		transform.position = new Vector3 (targetX, transform.position.y, transform.position.z);
+		float rotationAngle = 180.0f * (currentX-finalX)/6.0f;
+		transform.eulerAngles = new Vector3 (transform.eulerAngles.x, rotationAngle, transform.eulerAngles.z);
 	}
 
 	private void ClampPositionToScreen() {
@@ -90,5 +98,6 @@ public class ShipMove : MonoBehaviour {
 	public void InitializeShip() {
 		transform.position = new Vector3 (initialPosition.x, initialPosition.y, initialPosition.z);
 		transform.rotation = Quaternion.identity;
+		xTar = 0.5f;
 	}
 }
